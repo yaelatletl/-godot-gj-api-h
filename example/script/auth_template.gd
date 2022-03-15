bd80b19968dfb52b1d0e3509fd33fae00470332c
Vincent D <40196601+deakcor@users.noreply.github.com>
2022-03-15 21:40:33 +0100

    remove example

--- example/script/auth_template.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-#copy this template file into a auth.gd in the same folder and name the class 'Auth'
-
-class_name AuthTemplate
-
-var private_key="" #the private key of your game
-var game_id="" #the game id
